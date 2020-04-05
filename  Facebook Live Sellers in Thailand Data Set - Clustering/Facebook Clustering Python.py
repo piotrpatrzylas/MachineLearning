12f70c7a6cde20397658130a2ba2c195ed387e47
Piotr Patrzylas <piotrpatrzylas@gmail.com>
2020-04-05 22:18:26 +0100

    Init

---  Facebook Live Sellers in Thailand Data Set - Clustering/Facebook Clustering Python.py
+++  Facebook Live Sellers in Thailand Data Set - Clustering/Facebook Clustering Python.py
@@ -1,1 +1,9 @@
 import numpy as np
+import pandas as pd
+
+# Data pre-processing
+df = pd.read_csv("Live.csv")
+pd.set_option('display.max_columns', None)
+df.head()
+df = df.drop(["Column1", "Column2", "Column3", "Column4"], axis=1)
+df.shape
